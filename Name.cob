b8124af26aa024cd3be5caad13b0d2831bb2508a
Satish Guduru <57361693+satishgsk87@users.noreply.github.com>
2024-12-25 11:34:00 +0530

    Update Name.cob

--- Name.cob
+++ Name.cob
@@ -1,43 +1,27 @@
- ******************************************************************
-      * Author:Satish
-      * Date:18/09/2017
-      * Purpose: Display the name 
-      * 
-      ******************************************************************
-       IDENTIFICATION DIVISION.
-      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-       PROGRAM-ID. HELLO.
-       ENVIRONMENT DIVISION.
-      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-       CONFIGURATION SECTION.
-      *-----------------------
-       INPUT-OUTPUT SECTION.
-      *-----------------------
-       DATA DIVISION.
-      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-       FILE SECTION.
-      *-----------------------
-       WORKING-STORAGE SECTION.
-      .
-       01 User-Name.
-           05 First-Name PIC X(10).
-           05 Last-Name  PIC X(10).
+      IDENTIFICATION DIVISION.
+      PROGRAM-ID. HELLO.
+      ENVIRONMENT DIVISION.
+      CONFIGURATION SECTION.
+      INPUT-OUTPUT SECTION.
+      DATA DIVISION.
+      FILE SECTION.
+      WORKING-STORAGE SECTION.
+           01 User-Name.
+              05 First-Name PIC X(10).
+              05 Last-Name  PIC X(10).
+              01 WS-NAME PIC X(20).
+      PROCEDURE DIVISION.
+      MAIN-PROCEDURE.
+          Initialize User-name.
+          Move 'Satish' to First-Name.
+          Move 'Guduru' to Last-Name.
+          DISPLAY 'First Name: ' First-Name.
+          DISPLAY 'Last Name: ' Last-Name.
+          String First-Name DELIMITED BY ' '
+                 SPACE      DELIMITED BY SIZE
+                 Last-Name  DELIMITED BY ' '
+                 INTO WS-NAME
+          END-STRING.
+          DISPLAY WS-NAME.
+          STOP RUN.
 
-      *-----------------------
-       PROCEDURE DIVISION.
-
-      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-       MAIN-PROCEDURE.
-      **
-      * The main procedure of the program
-      **
-            Initialize User-name.
-            Move 'Satish' to First-Name.
-            Move 'Guduru' to Last-Name.
-            
-            DISPLAY 'First Name: ' First-Name.
-            DISPLAY 'Last Name: ' Last-Name.
-            DISPLAY User-Name.
-            STOP RUN.
-      ** add other procedures here
-       END PROGRAM HELLO.
